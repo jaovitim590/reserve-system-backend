cb5afb415cfd5a39383d2a5f2e49d2766d00b978
jaovitim590 <joaorodriguesjv590@gmail.com>
2026-01-27 23:29:47 -0300

    first repositories

---
+++ src/main/java/com/reservSystem/ReservSystem/repositories/QuartoRepository.java
@@ -0,0 +1,12 @@
+package com.reservSystem.ReservSystem.repositories;
+
+import com.reservSystem.ReservSystem.models.Quarto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuartoRepository extends JpaRepository<Quarto, Integer> {
+    List<Quarto> findAllByStatus(String status);
+}
---
+++ src/main/java/com/reservSystem/ReservSystem/repositories/ReservaRepository.java
@@ -0,0 +1,9 @@
+package com.reservSystem.ReservSystem.repositories;
+
+import com.reservSystem.ReservSystem.models.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
+}
