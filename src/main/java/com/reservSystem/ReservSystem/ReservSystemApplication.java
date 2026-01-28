ffd0a628a5d2b5340adf3360e53b7906f60e3dbb
jaovitim590 <joaorodriguesjv590@gmail.com>
2026-01-27 21:23:13 -0300

    first commit

---
+++ src/main/java/com/reservSystem/ReservSystem/ReservSystemApplication.java
@@ -0,0 +1,13 @@
+package com.reservSystem.ReservSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservSystemApplication.class, args);
+	}
+
+}
